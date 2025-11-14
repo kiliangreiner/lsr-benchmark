66b314ff46bb16e58073e127ceae3ea6419542cf
Maik Fröbe <mam10eks@users.noreply.github.com>
2025-11-14 14:22:09 +0100

    Refactor main function to include retrieval option

--- step-03-retrieval-approaches/lexical/pyterrier-naive/run-pyterrier.py
+++ step-03-retrieval-approaches/lexical/pyterrier-naive/run-pyterrier.py
@@ -9,16 +9,18 @@ from shutil import rmtree
 import pandas as pd
 from tira.third_party_integrations import ensure_pyterrier_is_loaded,  normalize_run
 import ir_datasets
-from lsr_benchmark.click import retrieve_command
+from lsr_benchmark.click import  option_lsr_dataset, option_retrieval_depth
 
-@retrieve_command()
-def main(dataset, output, embedding, k):
+@option_lsr_dataset()
+@option_retrieval_depth()
+@click.option("--retrieval", type=str, required=False, default="BM25", help="The retrieval model to use.")
+def main(dataset, output, retrieval, k):
     output.mkdir(parents=True, exist_ok=True)
     lsr_benchmark.register_to_ir_datasets(dataset)
     ir_dataset = ir_datasets.load(f"lsr-benchmark/{dataset}")
     ensure_pyterrier_is_loaded(boot_packages=())
 
-    register_metadata({"actor": {"team": "reneuir-baselines"}, "tag": f"pyterrier-naive-{embedding.lower()}-top-{k}"})
+    register_metadata({"actor": {"team": "reneuir-baselines"}, "tag": f"pyterrier-naive-{retrieval.lower()}-top-{k}"})
     documents = [{"docno": i.doc_id, "text": i.default_text()} for i in ir_dataset.docs_iter()]
 
     with tracking(export_file_path=output / "index-metadata.yml", export_format=ExportFormat.IR_METADATA):
@@ -34,7 +36,7 @@ def main(dataset, output, embedding, k):
     for i in ir_dataset.queries_iter():
         queries.extend([{"qid": i.query_id, "query": pt_tokenize(i.default_text())}])
 
-    pipeline = pt.terrier.Retriever(index, wmodel=embedding)
+    pipeline = pt.terrier.Retriever(index, wmodel=retrieval)
     with tracking(export_file_path=output / "retrieval-metadata.yml", export_format=ExportFormat.IR_METADATA):
         run = pipeline(pd.DataFrame(queries))
 
