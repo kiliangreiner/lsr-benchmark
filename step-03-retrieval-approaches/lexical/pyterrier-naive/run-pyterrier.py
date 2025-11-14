a669e8c9010e9aeb5d7bd821c0b26b3edd0136fb
Maik Fröbe <mam10eks@users.noreply.github.com>
2025-11-14 14:07:15 +0100

    Update run-pyterrier.py

--- step-03-retrieval-approaches/lexical/pyterrier-naive/run-pyterrier.py
+++ step-03-retrieval-approaches/lexical/pyterrier-naive/run-pyterrier.py
@@ -9,26 +9,16 @@ from shutil import rmtree
 import pandas as pd
 from tira.third_party_integrations import ensure_pyterrier_is_loaded,  normalize_run
 import ir_datasets
-from lsr_benchmark.utils import ClickParamTypeLsrDataset
+from lsr_benchmark.click import retrieve_command
 
-@click.command()
-@click.option(
-    "--dataset",
-    type=ClickParamTypeLsrDataset(),
-    required=True,
-    help="The dataset id or a local directory.",
-)
-@click.option("--output", required=True, type=Path, help="The directory where the output should be stored.",
-)
-@click.option("--retrieval", type=str, required=False, default="BM25", help="The retrieval model.")
-@click.option("--k", type=int, required=False, default=10, help="The retrieval depth.")
-def main(dataset, output, retrieval, k):
+@retrieve_command()
+def main(dataset, output, embedding, k):
     output.mkdir(parents=True, exist_ok=True)
     lsr_benchmark.register_to_ir_datasets(dataset)
     ir_dataset = ir_datasets.load(f"lsr-benchmark/{dataset}")
     ensure_pyterrier_is_loaded(boot_packages=())
 
-    register_metadata({"actor": {"team": "reneuir-baselines"}, "tag": f"pyterrier-naive-{retrieval.lower()}-top-{k}"})
+    register_metadata({"actor": {"team": "reneuir-baselines"}, "tag": f"pyterrier-naive-{embedding.lower()}-top-{k}"})
     documents = [{"docno": i.doc_id, "text": i.default_text()} for i in ir_dataset.docs_iter()]
 
     with tracking(export_file_path=output / "index-metadata.yml", export_format=ExportFormat.IR_METADATA):
@@ -44,7 +34,7 @@ def main(dataset, output, retrieval, k):
     for i in ir_dataset.queries_iter():
         queries.extend([{"qid": i.query_id, "query": pt_tokenize(i.default_text())}])
 
-    pipeline = pt.terrier.Retriever(index, wmodel=retrieval)
+    pipeline = pt.terrier.Retriever(index, wmodel=embedding)
     with tracking(export_file_path=output / "retrieval-metadata.yml", export_format=ExportFormat.IR_METADATA):
         run = pipeline(pd.DataFrame(queries))
 
